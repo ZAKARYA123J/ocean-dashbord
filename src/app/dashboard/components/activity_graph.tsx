1ce41d4e55361bfbace7b86e0db847c87ebc388e
SupremeDeity <29257166+SupremeDeity@users.noreply.github.com>
2023-10-03 14:36:41 +0500

    fix: try  fix overflow

--- src/app/dashboard/components/activity_graph.tsx
+++ src/app/dashboard/components/activity_graph.tsx
@@ -25,7 +25,7 @@ export default function ActivityGraph({ data }: { data: CalendarDatum[] }) {
       tooltip={(val) => {
         return (
           // ! HOTFIX for nivo tooltip jitter bug - [tw-absolute tw-min-w-max -tw-translate-x-1/2 -tw-translate-y-full]
-          <div className="tw-absolute !tw-z-40 tw-block tw-min-w-max -tw-translate-x-full -tw-translate-y-full tw-break-words tw-rounded-md tw-bg-stone-200 tw-p-4 dark:tw-bg-slate-700">
+          <div className="tw-absolute !tw-z-40 tw-block -tw-translate-x-full -tw-translate-y-full tw-break-words tw-rounded-md tw-bg-stone-200 tw-p-4 dark:tw-bg-slate-700">
             <span
               className={`tw-select-none tw-px-6 tw-py-1 tw-font-semibold`}
               style={{ backgroundColor: val.color }}
