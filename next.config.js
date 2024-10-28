6bc27c64edbaa14a5883c2f3b604b1465aab767a
ds <zakaryabaouali255@gmail.com>
2024-10-28 10:43:29 +0100

    fdf

--- next.config.js
+++ next.config.js
@@ -7,7 +7,21 @@ const nextConfig = {
         destination: "/dashboard/devis",
         permanent: false,
       },
-    ];  
+    ];
+  },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this with your actual origin
+          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+    ];
   },
   images: {
     remotePatterns: [
@@ -17,22 +31,6 @@ const nextConfig = {
       },
     ],
   },
-};
-module.exports = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  }
 };
 
 module.exports = nextConfig;
