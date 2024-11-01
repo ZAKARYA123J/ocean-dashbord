dde5727554f1ac713321e9e238f6f49be363487e
ds <zakaryabaouali255@gmail.com>
2024-11-01 12:40:45 +0100

    fdfd

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useAuth } from '../AuthContext';
 import styled from 'styled-components';
-
+console.log('dfd')
 function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
